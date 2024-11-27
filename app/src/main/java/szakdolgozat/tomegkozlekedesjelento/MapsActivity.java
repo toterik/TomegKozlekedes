0d7ec559b1b7ded8af9c9d6e70d09e7f93c68f19
Tóth Erik <totherik420@gmail.com>
2024-11-27 13:19:37 +0100

    A térkép megnyitásakor megjelenik az összes Marker

--- app/src/main/java/szakdolgozat/tomegkozlekedesjelento/MapsActivity.java
+++ app/src/main/java/szakdolgozat/tomegkozlekedesjelento/MapsActivity.java
@@ -4,24 +4,38 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.MetadataChanges;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 
 import szakdolgozat.tomegkozlekedesjelento.databinding.ActivityMapsBinding;
 
@@ -58,17 +72,47 @@ public class MapsActivity extends MenuForAllActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        boolean userIsLoggedIn = user != null;
         mMap = googleMap;
-        LatLng hungary = new LatLng(47.1625, 19.5033);
-        mMap.addMarker(new MarkerOptions().position(hungary).title("Marker in Hungary"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hungary,10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(47.1625, 19.5033),10));
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
 
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        addMarkerToDatabaseOnMapClick();
+
+        displayAllMarkers();
+    }
+
+
+
+
+    public void displayAllMarkers() {
+        var markers = db.collection("markers");
+        markers.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                Map<String, Object> currentItem = document.getData();
+                String uid = currentItem.get("uid").toString();
+                double latitude = (Double) currentItem.get("latitude");
+                double longitude = (Double) currentItem.get("longitude");
+
+                LatLng latLng = new LatLng(latitude, longitude);
+
+                mMap.addMarker(new MarkerOptions().position(latLng));
+            }
+        }).addOnFailureListener(e ->
+        {
+            Log.e("Marker_Fail", "Error fetching markers", e);
+        });
+
+    }
+
+
+    public void addMarkerToDatabaseOnMapClick()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        boolean userIsLoggedIn = user != null;
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
+        {
             @Override
             public void onMapClick(LatLng point)
             {
@@ -80,24 +124,24 @@ public class MapsActivity extends MenuForAllActivity implements OnMapReadyCallba
                     data.put("uid", user.getUid());
 
                     db.collection("markers")
-                        .add(data)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>()
-                        {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference)
+                            .add(data)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>()
                             {
-                                Toast.makeText(MapsActivity.this, "Successfully added Marker!", Toast.LENGTH_SHORT).show();
-                                mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude,point.longitude)));
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener()
-                        {
-                            @Override
-                            public void onFailure(@NonNull Exception e)
+                                @Override
+                                public void onSuccess(DocumentReference documentReference)
+                                {
+                                    Toast.makeText(MapsActivity.this, "Successfully added Marker!", Toast.LENGTH_SHORT).show();
+                                    mMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude,point.longitude)));
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener()
                             {
-                                Toast.makeText(MapsActivity.this, "Error adding marker!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                                @Override
+                                public void onFailure(@NonNull Exception e)
+                                {
+                                    Toast.makeText(MapsActivity.this, "Error adding marker!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                 }
                 else
                 {
